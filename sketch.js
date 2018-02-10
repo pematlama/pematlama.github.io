dd2c92dc1a397cf09567e90d5612e520f5602590
pematlama <pemathilen@hotmail.com>
2018-02-10 17:33:38 -0500

    Updated Sketch

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+var xLocation = 100;
+var yLocation = 100;
+var xSpeed = 0;
+var ySpeed = 0;
 function setup() {
   createCanvas(600, 600);
 }
 
+function keyPressed() {
+  if(keyCode == RIGHT_ARROW) {
+    xSpeed = xSpeed + 5;
+  }
+  if(keyCode == UP_ARROW) {
+    ySpeed = ySpeed - 5;
+  }
+  if(keyCode == LEFT_ARROW) {
+    xSpeed = xSpeed - 5;
+  }
+  if(keyCode == DOWN_ARROW) {
+    ySpeed = ySpeed + 5;
+  }
+}
+
+
 function draw() {
-  ellipse(100, 100, 50, 50);
+  background(27, 158, 119);
+  fill(255, 0, 0);
+  stroke(0, 0, 255);
+  strokeWeight(4);
+  ellipse(xLocation, yLocation, 50, 50);
+
+  xLocation = xLocation + xSpeed;
+  yLocation = yLocation + ySpeed;
+
+  textSize(18);
+  strokeWeight(1);
+  text(xSpeed, 100, 20);
+  text(ySpeed, 150, 20);
 }
